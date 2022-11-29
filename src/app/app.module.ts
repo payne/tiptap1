bdf6d30ed7b398512c9be562d5d7f03d27be6e47
Matt Payne <Payne@MattPayne.org>
2022-11-28 20:47:37 -0600

    imported the module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
+import { NgxTiptapModule } from 'ngx-tiptap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -9,7 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,NgxTiptapModule,FormsModule,
     AppRoutingModule
   ],
   providers: [],
