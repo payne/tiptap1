60ab27769fec57ef0df642912c1f7938a8112e67
Matt Payne <Payne@MattPayne.org>
2022-11-28 20:53:30 -0600

    It is working from instructions at https://www.npmjs.com/package/ngx-tiptap

--- src/app/tt-try1/tt-try1.component.ts
+++ src/app/tt-try1/tt-try1.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Editor } from '@tiptap/core';
+import StarterKit from '@tiptap/starter-kit';
+
 
 @Component({
   selector: 'app-tt-try1',
@@ -6,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./tt-try1.component.scss']
 })
 export class TtTry1Component {
+
+  editor = new Editor({ extensions: [StarterKit] });
+  value = '<p>Hello, Tiptap!</p>'; // can be HTML or JSON, see https://www.tiptap.dev/api/editor#content
+
+  ngOnDestroy(): void {
+    this.editor.destroy();
+  }
 
 }
