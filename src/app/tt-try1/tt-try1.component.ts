13753e5a4221cfc76ca8fdcf5ec32be559fc5d26
Matt Payne <Payne@MattPayne.org>
2022-11-28 21:06:51 -0600

    This installs some of the dependencies for using websockets instead of WebRTC.
    WebRTC is working locally between FireFox and Chrome on my Mac MINI
    
    :thinking:

--- src/app/tt-try1/tt-try1.component.ts
+++ src/app/tt-try1/tt-try1.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { Editor } from '@tiptap/core';
 import StarterKit from '@tiptap/starter-kit';
 
+import * as Y from 'yjs'
+import { WebrtcProvider } from 'y-webrtc'
+import {Collaboration} from "@tiptap/extension-collaboration";
+
 
 @Component({
   selector: 'app-tt-try1',
@@ -9,8 +13,14 @@ import StarterKit from '@tiptap/starter-kit';
   styleUrls: ['./tt-try1.component.scss']
 })
 export class TtTry1Component {
-
-  editor = new Editor({ extensions: [StarterKit] });
+// A new Y document
+  ydoc = new Y.Doc()
+// Registered with a WebRTC provider
+  provider = new WebrtcProvider('example-document', this.ydoc)
+  editor = new Editor({ extensions: [
+    StarterKit.configure({ history: false}),
+      Collaboration.configure({ document: this.ydoc})
+  ] });
   value = '<p>Hello, Tiptap!</p>'; // can be HTML or JSON, see https://www.tiptap.dev/api/editor#content
 
   ngOnDestroy(): void {
